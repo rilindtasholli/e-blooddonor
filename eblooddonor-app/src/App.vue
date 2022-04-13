2d9c1ce34c315dbf83a8ba7b9d7e537e0a77f78a
rilindtasholli <rilindtasholli@gmail.com>
2022-04-14 01:01:22 +0200

    added vue-router

--- eblooddonor-app/src/App.vue
+++ eblooddonor-app/src/App.vue
@@ -2,6 +2,9 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+    <router-view></router-view>
   </div>
 </template>
 
