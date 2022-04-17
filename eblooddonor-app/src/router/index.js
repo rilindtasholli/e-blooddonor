34bc87bdc23c04ce90279fd46233b16eaa3961d2
Besart Ibishi <bi47125@ubt-uni.net>
2022-04-17 20:57:50 +0200

    Added register page

--- eblooddonor-app/src/router/index.js
+++ eblooddonor-app/src/router/index.js
@@ -1,57 +1,73 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Vue from "vue";
+import VueRouter from "vue-router";
 
-import Home from '@/views/Home'
-import User from '@/views/User'
+import Home from "@/views/Home";
+import User from "@/views/User";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '',
-    redirect: '/home'
-  },
-  { 
-    path: '/home',
-    component: Home,
-    children: [
-      { 
-        path: "",
-        component: () => import(/* webpackChunkName: "home" */ "../views/HomePages/HomePage.vue"),
-      },
-      { 
-        path: "announcement",
-        component: () => import(/* webpackChunkName: "announcement" */ "../views/HomePages/AnnouncementShow.vue"),
-      },
-    ]
-  },
-  { 
-    path: "/about",
-     // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    path: "",
+    redirect: "/home",
   },
   {
-    path: '/user/',
-    component: User,
-    redirect: '/user/profile',
+    path: "/home",
+    component: Home,
     children: [
       {
-        path: 'profile',
+        path: "",
+        component: () =>
+          import(
+            /* webpackChunkName: "home" */ "../views/HomePages/HomePage.vue"
+          ),
+      },
+      {
+        path: "announcement",
+        component: () =>
+          import(
+            /* webpackChunkName: "announcement" */ "../views/HomePages/AnnouncementShow.vue"
+          ),
+      },
+    ],
+  },
+  {
+    path: "/about",
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+  {
+    path: "/user/register",
+    name: "Register",
+    component: () =>
+      import(/* webpackChunkName: "register" */ "../views/Register.vue"),
+  },
+
+  {
+    path: "/user/login",
+    name: "Login",
+    component: () =>
+      import(/* webpackChunkName: "register" */ "../views/Login.vue"),
+  },
+
+  {
+    path: "/user/",
+    component: User,
+    redirect: "/user/profile",
+    children: [
+      {
+        path: "profile",
         // component: () => import(/* webpackChunkName: "user" */ "../views/Profile.vue"),
       },
       {
-        path: 'register',
-        // component: () => import(/* webpackChunkName: "user" */ "../views/Register.vue"),
-      },
-  
-      {
-        path: 'login',
+        path: "login",
         // component: () => import(/* webpackChunkName: "user" */ "../views/Login.vue"),
-      }
-    ]
-  }
-]
+      },
+    ],
+  },
+];
 
 const router = new VueRouter({
   mode: "history",
@@ -59,4 +75,4 @@ const router = new VueRouter({
   routes,
 });
 
-export default router
+export default router;
---
+++ eblooddonor-app/src/views/Login.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores ipsam sit
+    cumque dolorem laborum ad, nemo corrupti esse consequatur, repudiandae
+    tempora fugiat provident, dolor ipsa eos. Ratione, tempora! Inventore, non.
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
